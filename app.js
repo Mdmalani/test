983c4aba4bf6098ea7cb7e122bf7e99fa7b910ec
Mdmalani <mdmalani30@gmail.com>
2020-04-25 22:05:47 +0530

    the fist

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT||5000
+
+app.get('/test', (req, res) => res.send(
+    {
+        "message":'Hello World!'
+    }
+    ))
+
+    
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static('client/build'))
+    const path=require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,
+            'client','build','index.html'
+        ))
+    })
+}
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

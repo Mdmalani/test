77adc474cad5d2ef3fe97e4ec390206017cd3dc6
Mdmalani <mdmalani30@gmail.com>
2020-04-26 16:07:49 +0530

    final app

--- app.js
+++ app.js
@@ -21,16 +21,12 @@ app.get('/test', (req, res) => res.send(
     })
 } */
 
-// if(process.env.NODE_ENV === 'production'){
-//     //set a static folder
-//     app.use(express.static('client/build'));
-//     app.get('*', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'));
-//     });
-// }
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
+if(process.env.NODE_ENV === 'production'){
+    //set a static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
